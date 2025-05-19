0c88df6a11f890f346edfae6e4e926c3f923440d
Syimyk Kalykov <syimykkalykov3@gmail.com>
2025-05-18 22:45:53 -0500

    fix: define border color in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        border: 'hsl(var(--border))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        accent: 'hsl(var(--accent))',
+        input: 'hsl(var(--input))',
+      },
+    },
   },
   plugins: [],
 }
-
